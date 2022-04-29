72787c576161124c45ffeb6fb0a92877bc14f284
oren hayoun <orenhayoun0@gmail.com>
2022-04-29 19:00:24 +0300

    Initial Commit

---
+++ Ex_4.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ex_4.c
+//
+//  Created by oren hayoun on 29/04/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
